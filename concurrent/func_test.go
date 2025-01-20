6a5b304c29c4eae53f83fd1ae426b8f2c55c5a49
Steven Wen <wzq961021@gmail.com>
2025-01-20 23:13:26 +0800

    feat: test goroutine print by order via channel

---
+++ concurrent/func_test.go
@@ -0,0 +1,7 @@
+package concurrent
+
+import "testing"
+
+func TestPrintAnimals(t *testing.T) {
+	printAnimals()
+}
---
+++ concurrent/goroutine_print.go
@@ -0,0 +1,49 @@
+package concurrent
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printAnimals() {
+	var wg sync.WaitGroup
+	ch1 := make(chan struct{})
+	ch2 := make(chan struct{})
+	ch3 := make(chan struct{})
+
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			<-ch1
+			fmt.Println("cat")
+			ch2 <- struct{}{}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			<-ch2
+			fmt.Println("dog")
+			ch3 <- struct{}{}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			<-ch3
+			fmt.Println("fish")
+			if i < 99 {
+				ch1 <- struct{}{}
+			}
+		}
+	}()
+
+	// Start the first goroutine
+	ch1 <- struct{}{}
+
+	wg.Wait()
+}
